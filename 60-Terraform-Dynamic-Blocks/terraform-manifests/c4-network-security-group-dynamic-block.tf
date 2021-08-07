1e67145a3af2cbb235a35d907f9b645193772b7a
STACKSIMPLIFY <stacksimplify@gmail.com>
2021-08-07 14:07:31 +0530

    Updated source_port_range in dymaic blocks to *
    Updated source_port_range in dymaic blocks to *

--- 60-Terraform-Dynamic-Blocks/terraform-manifests/c4-network-security-group-dynamic-block.tf
+++ 60-Terraform-Dynamic-Blocks/terraform-manifests/c4-network-security-group-dynamic-block.tf
@@ -19,7 +19,7 @@ resource "azurerm_network_security_group" "mynsg2" {
       direction                  = "Inbound"
       access                     = "Allow"
       protocol                   = "Tcp"
-      source_port_range          = security_rule.value
+      source_port_range          = "*"
       destination_port_range     = security_rule.value
       source_address_prefix      = "*"
       destination_address_prefix = "*"      
